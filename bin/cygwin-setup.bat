f1014f7874e6f0654b0533497c45fe8a4732aa30
Tom Schutter <t.schutter@comcast.net>
2012-07-25 09:26:28 -0600

    cygwin-setup.bat: call setup 2nd time to update installed packages

--- bin/cygwin-setup.bat
+++ bin/cygwin-setup.bat
@@ -50,7 +50,9 @@ goto :eof
 goto :eof
 
 :setup
-    echo %_PREFIX% Running setup.exe
+    echo %_PREFIX% Running setup.exe to install standard package set
+    %_ROOTDIR%\setup.exe --site %_SITE% --quiet-mode --no-shortcuts --root %_ROOTDIR% --local-package-dir %_ROOTDIR%\LocalPackageDir --packages %_PACKAGES%
+    echo %_PREFIX% Running setup.exe for updates
     %_ROOTDIR%\setup.exe --site %_SITE% --quiet-mode --no-shortcuts --root %_ROOTDIR% --local-package-dir %_ROOTDIR%\LocalPackageDir --packages %_PACKAGES%
 goto :eof
 
