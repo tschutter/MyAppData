b9d525075fe839fddf4ddf6301d83dac8abb4daf
Tom Schutter <t.schutter@comcast.net>
2012-08-17 21:55:34 -0600

    windows_config.bat: add check for Python

--- bin/windows_config.bat
+++ bin/windows_config.bat
@@ -1,3 +1,12 @@
 @echo off
 
-python "%~dp0/windows_config.py" %*
+setlocal
+
+rem Check if Python is installed.
+for /f %%i in ("python.exe") do set _FOUND=%%~$PATH:i
+if not "%_FOUND%" == "" goto :endif
+    echo ERROR: Python not installed.  See http://python.org/download/
+    goto :eof
+:endif
+
+python.exe "%~dp0/windows_config.py" %*
