c5c29eae63540badc612c04271ae56d16f6d46a1
Tom Schutter <t.schutter@comcast.net>
2012-08-18 10:36:15 -0600

    cygwin_setup.bat: genisoimage,wodim are no longer critical _PACKAGES

--- bin/cygwin_setup.bat
+++ bin/cygwin_setup.bat
@@ -46,9 +46,11 @@ rem
 setlocal
 
 rem Configuration.
-set _PACKAGES=cron,diffutils,inetutils,ncurses,netcat,openssh,p7zip,procps,python,rsync,screen,unzip,vim,zip,genisoimage,w3m,wodim
+set _PACKAGES=cron,diffutils,inetutils,ncurses,netcat,openssh,p7zip,procps,python,rsync,screen,unzip,vim,zip,w3m
 set _ROOTDIR=C:\cygwin
 set _SITE=http://mirrors.xmission.com/cygwin
+
+rem Global constants.
 set _PREFIX=CYGWIN_SETUP:
 
 goto :main
