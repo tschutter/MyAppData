1219c9dce35367301206651920dd6cd40c281544
tschutter <t.schutter@comcast.net>
2012-08-27 18:56:34 -0600

    cygwin_setup_config.bat: add comments

--- bin/cygwin_setup_config.bat
+++ bin/cygwin_setup_config.bat
@@ -2,6 +2,13 @@ rem cygwin_setup.bat configuration.
 set _PACKAGES=cron,diffutils,inetutils,ncurses,netcat,openssh,p7zip,procps,python,rsync,screen,unzip,vim,zip,w3m
 set _ROOTDIR=C:\cygwin
 set _SITE=http://mirrors.xmission.com/cygwin
+
+rem Enable logging of sshd and it's ilk to /var/log.
 set _CONFIG_SYSLOGD=True
+
+rem Enable OpenSSH server.
 set _CONFIG_SSHD=True
+
+rem Enable LSA authentication.
+rem See http://cygwin.com/cygwin-ug-net/ntsec.html
 set _CONFIG_LSA=True
