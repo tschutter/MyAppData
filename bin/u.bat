f769e7de1323a620d8f67652509a28ddaafb1dc8
Tom Schutter <t.schutter@comcast.net>
2012-12-02 16:05:04 -0700

    LICENSE.txt: initial checkin

--- bin/u.bat
+++ bin/u.bat
@@ -2,33 +2,6 @@
 rem
 rem Directory tree navigation
 rem
-rem Copyright (c) 2006-2012 Tom Schutter
-rem All rights reserved.
-rem
-rem Redistribution and use in source and binary forms, with or without
-rem modification, are permitted provided that the following conditions
-rem are met:
-rem
-rem    - Redistributions of source code must retain the above copyright
-rem      notice, this list of conditions and the following disclaimer.
-rem    - Redistributions in binary form must reproduce the above
-rem      copyright notice, this list of conditions and the following
-rem      disclaimer in the documentation and/or other materials provided
-rem      with the distribution.
-rem
-rem THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-rem "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-rem LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-rem FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-rem COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-rem INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-rem BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-rem LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-rem CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-rem LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-rem ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-rem POSSIBILITY OF SUCH DAMAGE.
-rem
 
 set _DIR_=..
 set _ARGS_=%*
