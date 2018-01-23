c3b346b9842fc2065ba928f34c16b3c1642f7f6a
Tom Schutter <tom@PRA-EliteBook1.pra.local>
2018-01-22 18:08:21 -0700

    windows_config.bat: fix test for Python3

--- bin/windows_config.bat
+++ bin/windows_config.bat
@@ -2,11 +2,18 @@
 
 setlocal
 
-rem Check if Python3 is installed.
-for /f %%i in ("python3.exe") do set _FOUND=%%~$PATH:i
+rem Locate python.exe.
+for /f %%i in ("python.exe") do set _FOUND=%%~$PATH:i
 if not "%_FOUND%" == "" goto :endif
-    echo ERROR: Python3 not installed.  See http://python.org/download/
+    echo ERROR: python.exe not found.
     goto :eof
 :endif
 
-python3.exe "%~dp0/windows_config.py" %*
+rem Determine if python.exe is Python3.
+"%_FOUND%" --version | findstr /B /L /C:"Python 3" >NUL 2>&1
+if not ERRORLEVEL 1 goto :endif2
+    echo ERROR: python.exe is not Python3.
+    goto :eof
+:endif2
+
+rem python.exe "%~dp0windows_config.py" %*
