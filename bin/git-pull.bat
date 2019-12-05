1139a8846bf85d230fac9ec86741f74784bda7db
Tom Schutter <t.schutter@platte.com>
2019-12-05 15:45:31 -0700

    git-pull.bat: do not pop stash if push did nothing

--- bin/git-pull.bat
+++ bin/git-pull.bat
@@ -3,6 +3,11 @@ rem
 rem Do a "git pull" wrapped with a stash push/pop.
 rem
 
-git stash push --keep-index --message "git-pull temp" --quiet
+git stash push --keep-index --message "git-pull temp"^
+  | findstr /C:"No local changes to save"^
+  > NUL:
+set STASHED=%ERRORLEVEL%
+
 git pull
-git stash pop --quiet
+
+if %STASHED% EQU 1 git stash pop --quiet
